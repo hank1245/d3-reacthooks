f638d4153624511bb03df1b9d0b723fa5f643436
Hank Kim <hank29206880@gmail.com>
2022-10-24 14:02:23 +0900

    interactrive

--- src/App.js
+++ src/App.js
@@ -1,55 +1,86 @@
-import logo from "./logo.svg";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
-import {
-  select,
-  line,
-  axisBottom,
-  scaleBand,
-  scaleLinear,
-  max,
-  axisRight,
-  curveCardinal,
-} from "d3";
+import { select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
 
 function App() {
-  const [data, setData] = useState([25, 100, 45, 60, 20, 30, 40]);
-  const svgRef = useRef(null);
-  useEffect(() => {
-    const svg = select(svgRef.current).attr("width", 300).attr("height", 150);
-    const width = svg.attr("width");
-    const height = svg.attr("height");
-    //scale
-    const xAxisG = svg.append("g").attr("transform", `translate(0,${height})`);
-    const yAxisG = svg.append("g").attr("transform", `translate(${width},0)`);
-    const xScale = scaleLinear().domain([0, 6]).range([0, width]);
-    const yScale = scaleLinear().domain([0, height]).range([height, 0]);
-    const xAxis = axisBottom(xScale).ticks(data.length);
-    const yAxis = axisRight(yScale);
-    xAxisG.call(xAxis);
-    yAxisG.call(yAxis);
-    //line
-    const myLine = line()
-      .x((d, i) => xScale(i))
-      .y(yScale)
-      .curve(curveCardinal);
+  const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
+  const svgRef = useRef();
 
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    // scales
+    const xScale = scaleBand()
+      .domain(data.map((value, index) => index))
+      .range([0, 300])
+      .padding(0.5);
+
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+
+    const colorScale = scaleLinear()
+      .domain([75, 100, 150])
+      .range(["green", "orange", "red"])
+      .clamp(true);
+
+    // create x-axis
+    const xAxis = axisBottom(xScale).ticks(data.length);
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+
+    // create y-axis
+    const yAxis = axisRight(yScale);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    // draw the bars
     svg
-      .selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("class", "line")
-      .attr("d", (d) => {
-        return myLine(d);
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .style("transform", "scale(1, -1)")
+      .attr("x", (value, index) => xScale(index))
+      .attr("y", -150)
+      .attr("width", xScale.bandwidth())
+      .on("mouseenter", (event, value) => {
+        // events have changed in d3 v6:
+        // https://observablehq.com/@d3/d3v6-migration-guide#events
+        const index = svg.selectAll(".bar").nodes().indexOf(event.target);
+        svg
+          .selectAll(".tooltip")
+          .data([value])
+          .join((enter) => enter.append("text").attr("y", yScale(value) - 4))
+          .attr("class", "tooltip")
+          .text(value)
+          .attr("x", xScale(index) + xScale.bandwidth() / 2)
+          .attr("text-anchor", "middle")
+          .transition()
+          .attr("y", yScale(value) - 8)
+          .attr("opacity", 1);
       })
-      .attr("fill", "none")
-      .attr("stroke", "blue");
+      .on("mouseleave", () => svg.select(".tooltip").remove())
+      .transition()
+      .attr("fill", colorScale)
+      .attr("height", (value) => 150 - yScale(value));
   }, [data]);
 
   return (
-    <div>
-      <svg ref={svgRef}></svg>
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 105))}>
+        Filter data
+      </button>
+      <button
+        onClick={() => setData([...data, Math.round(Math.random() * 100)])}
+      >
+        Add data
+      </button>
+    </React.Fragment>
   );
 }
 
